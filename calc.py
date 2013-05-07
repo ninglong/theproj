54ac6b7ae8b74c64bf2da7517849f094f26bde23
ninglong <ninglongxu@gmail.com>
2013-05-07 15:23:18 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -22,3 +22,4 @@ print 'the average is', avg, 'and sumsqdiffs is', sumsqdiffs
 print 'stddev is', stddev
 print 'sum sq diffs', sumsqdiffs #adding a comment
 # cloned from github.com
+print 'Added a small change'
